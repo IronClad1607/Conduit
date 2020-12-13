f02c08e7e8ae08e3ca4e0925f49d86aa80d97e16
Ishaan Jain <ishaan160799@gmail.com>
2020-12-13 16:45:41 +0530

    articles api done

--- api/src/main/java/com/ironclad/api/services/ConduitAPI.kt
+++ api/src/main/java/com/ironclad/api/services/ConduitAPI.kt
@@ -4,9 +4,16 @@ import com.ironclad.api.models.ArticlesResponse
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ConduitAPI {
 
     @GET("articles")
-    fun getArticles(): Call<ArticlesResponse>
+    suspend fun getArticles(
+        @Query("author") author: String? = null,
+        @Query("favourited") favourited: String? = null,
+        @Query("tag") tag: String? = null
+    ): Response<ArticlesResponse>
+
+
 }
--- api/src/test/java/com/ironclad/api/ConduitClientTests.kt
+++ api/src/test/java/com/ironclad/api/ConduitClientTests.kt
@@ -1,14 +1,34 @@
 package com.ironclad.api
 
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 
 class ConduitClientTests {
 
     private val conduitClient = ConduitClient()
+
     @Test
-    fun `GET articles`(){
-        val articles = conduitClient.conduitAPI.getArticles().execute()
-        assertNotNull(articles.body()?.articles)
+    fun `GET articles`() {
+        runBlocking {
+            val articles = conduitClient.conduitAPI.getArticles()
+            assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `GET articles by author`() {
+        runBlocking {
+            val articles = conduitClient.conduitAPI.getArticles(author = "444")
+            assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `GET articles by tags`() {
+        runBlocking {
+            val articles = conduitClient.conduitAPI.getArticles(tag = "dragons")
+            assertNotNull(articles.body()?.articles)
+        }
     }
 }
