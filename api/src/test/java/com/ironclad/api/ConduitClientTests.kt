db422030c12f4f66a6a677f8782f5379267af547
Ishaan Jain <ishaan160799@gmail.com>
2020-12-16 17:51:41 +0530

    ui set

--- api/src/test/java/com/ironclad/api/ConduitClientTests.kt
+++ api/src/test/java/com/ironclad/api/ConduitClientTests.kt
@@ -1,8 +1,12 @@
 package com.ironclad.api
 
+import com.ironclad.api.models.entities.UserCreds
+import com.ironclad.api.models.requests.SignUpRequest
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Test
+import kotlin.random.Random
 
 class ConduitClientTests {
 
@@ -29,6 +33,20 @@ class ConduitClientTests {
         runBlocking {
             val articles = conduitClient.conduitAPI.getArticles(tag = "dragons")
             assertNotNull(articles.body()?.articles)
+        }
+    }
+
+    @Test
+    fun `POST user - sign up`(){
+        val userCreds = UserCreds(
+            email = "testemail${Random.nextInt(999,9999)}@test.com",
+            password = "pass${Random.nextInt(9999,99999)}",
+            username = "rand_user_${Random.nextInt(99,9999)}"
+        )
+
+        runBlocking {
+            val resp = conduitClient.conduitAPI.signUpUser(SignUpRequest(userCreds))
+            assertEquals(userCreds.username,resp.body()?.user?.username)
         }
     }
 }
