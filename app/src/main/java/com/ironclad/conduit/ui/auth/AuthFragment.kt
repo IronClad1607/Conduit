06665e701c51d73fcdcef3285b3783aa56b93699
Ishaan Jain <ishaan160799@gmail.com>
2020-12-19 15:57:24 +0530

    auth fragment start

--- app/src/main/java/com/ironclad/conduit/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/ironclad/conduit/ui/auth/AuthFragment.kt
@@ -1,6 +1,27 @@
 package com.ironclad.conduit.ui.auth
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.ironclad.conduit.databinding.FragmentAuthBinding
 
 class AuthFragment : Fragment() {
+
+    private lateinit var binding: FragmentAuthBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAuthBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
 }
