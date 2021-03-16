e22803c4bcfa510b63a464e6a7173fcfe7b752de
Ishaan Jain <ishaan160799@gmail.com>
2021-03-16 18:12:20 +0530

    tab change listener

--- app/src/main/java/com/ironclad/conduit/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/ironclad/conduit/ui/auth/AuthFragment.kt
@@ -5,23 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import com.ironclad.conduit.databinding.FragmentAuthBinding
 
 class AuthFragment : Fragment() {
 
-    private lateinit var binding: FragmentAuthBinding
+    private var binding: FragmentAuthBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         binding = FragmentAuthBinding.inflate(inflater, container, false)
 
-        return binding.root
+        binding?.authTabLayout?.addOnTabSelectedListener((object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                TODO("Not yet implemented")
+            }
+
+        }))
+        return binding?.root
     }
 
     override fun onDestroyView() {
+        binding = null
         super.onDestroyView()
     }
 }
